6fec00bd337f90df05d2640ad8f72c349533fb65
Pavlos Smith <paulsmith4561@gmail.com>
2025-05-29 20:45:46 +0300

    Fixed main-content scrolling

--- src/App.vue
+++ src/App.vue
@@ -119,6 +119,7 @@ const filterFiles = (structure) => {
 }
 
 .top-bar {
+  /* flex-shrink: 0; */
   display: flex;
   align-items: center;
   padding: 10px;
@@ -141,15 +142,13 @@ const filterFiles = (structure) => {
 .main-layout {
   display: flex;
   flex: 1;
-  overflow: hidden;
-  width: 100%;
+  min-height: 0;
 }
 
 .sidebar {
   width: 500px;
   border-right: 1px solid #ccc;
   padding: 10px;
-  overflow-y: auto;
   display: flex;
   align-items: flex-start;
   justify-content: flex-start;
@@ -171,11 +170,15 @@ const filterFiles = (structure) => {
 .main-content {
   display: grid;
   padding: 20px;
-  overflow-y: auto;
   width: 100%;
   gap: 16px;
   grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
   grid-auto-rows: auto;
-  height: max-content;
 }
+
+.sidebar, .main-content {
+  overflow-y: auto;
+  height: 100%;
+}
+
 </style>
